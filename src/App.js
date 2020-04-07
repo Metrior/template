b8dc90c1db7a4bc2e0c327f9ca2ac25ce739ab4e
Metrior <warshavskiythebest@gmail.com>
2020-04-07 18:23:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {Provider} from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = ({store}) => (
+      <Provider store={store}>
+          <Router>
+              <Route path="/" component={}/>
+          </Router>
+      </Provider>
   );
-}
+
+App.propTypes = {
+    store: PropTypes.object.isRequired
+};
 
 export default App;
